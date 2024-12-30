d2c4b079cfb16e0be82b2f605b35da0a84507f36
Gilles Perreymond <gperreymond@gmail.com>
2024-12-30 16:40:33 +0100

    chore(jsonnet): contruction of alertmanager

--- scripts/jsonnet-build.sh
+++ scripts/jsonnet-build.sh
@@ -34,4 +34,18 @@ for file in $output_dir/prometheus-*.yaml; do
   fi
 done
 
+output_dir="charts/remote-charts/alert-manager/manifests"
+rm -rf $output_dir
+mkdir -p $output_dir
+mv build/manifests/alertmanager-*.yaml $output_dir
+echo "apiVersion: kustomize.config.k8s.io/v1beta1" > $output_dir/kustomization.yaml
+echo "kind: Kustomization" >> $output_dir/kustomization.yaml
+echo "resources:" >> $output_dir/kustomization.yaml
+for file in $output_dir/alertmanager-*.yaml; do
+  echo "[INFO] add $file to kustomization.yaml"
+  if [ -f "$file" ]; then
+    echo "  - $(basename "$file")" >> $output_dir/kustomization.yaml
+  fi
+done
+
 # rm -rf build
