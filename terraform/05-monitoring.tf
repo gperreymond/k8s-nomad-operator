26bab596c0b817962241725c2d639d26afb192c3
Gilles Perreymond <gperreymond@gmail.com>
2024-12-30 09:24:51 +0100

    chore(thanos): add kustomize

--- terraform/05-monitoring.tf
+++ terraform/05-monitoring.tf
@@ -14,10 +14,23 @@ YAML
 // KUBERNETES
 // ----------------------------
 
-resource "kubernetes_secret" "thanos_objstore_configuration" {
+resource "kubernetes_secret" "thanos_sidecar_objstore_configuration" {
   metadata {
     name      = "thanos-objstore-config"
     namespace = kubernetes_namespace.monitoring.id
+  }
+  data = {
+    "thanos.yaml" = local.thanos_objstore_configuration
+  }
+
+  depends_on = [
+    null_resource.kestra,
+  ]
+}
+resource "kubernetes_secret" "thanos_objstore_configuration" {
+  metadata {
+    name      = "thanos-objstore-config"
+    namespace = kubernetes_namespace.thanos.id
   }
   data = {
     "thanos.yaml" = local.thanos_objstore_configuration
@@ -32,6 +45,7 @@ resource "null_resource" "monitoring" {
   depends_on = [
     null_resource.kestra,
     // kubernetes
+    kubernetes_secret.thanos_sidecar_objstore_configuration,
     kubernetes_secret.thanos_objstore_configuration,
     // nomad
   ]
